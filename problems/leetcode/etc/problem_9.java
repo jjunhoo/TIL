142120231da648dfae6524a784dd2f1a43e63aa2
juno <wnsghi1@naver.com>
2024-04-23 21:39:35 +0900

    prob : leetcode 9

--- problems/leetcode/etc/problem_9.java
+++ problems/leetcode/etc/problem_9.java
@@ -4,5 +4,23 @@ package problems.leetcode.etc;
  * https://leetcode.com/problems/palindrome-number/
  */
 public class problem_9 {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));
+        System.out.println(isPalindrome(-121));
+        System.out.println(isPalindrome(10));
+    }
 
+    private static boolean isPalindrome(int x) {
+
+        char[] arr = String.valueOf(x).toCharArray();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] != arr[arr.length - i - 1]) {
+                return Boolean.FALSE;
+            }
+        }
+
+
+        return Boolean.TRUE;
+    }
 }
