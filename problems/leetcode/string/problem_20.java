40b5f6c2473de29efcdd736c4e493c6c82f58a7e
221075 <221075@ssg.com>
2024-06-18 08:54:11 +0900

    prob : leetcode 20

---
+++ problems/leetcode/string/problem_20.java
@@ -0,0 +1,31 @@
+package problems.leetcode.string;
+
+import java.util.Stack;
+
+/**
+ * https://leetcode.com/problems/valid-parentheses/description/
+ */
+public class problem_20 {
+    public static void main(String[] args) {
+        System.out.println(valid("([)]"));
+    }
+
+    /**
+     * 1. case
+     * - Input: s = "()"
+     * - Output: true
+     *
+     * 2. case
+     * - Input: s = "()[]{}"
+     * - Output: true
+     *
+     * 3. case
+     * - Input: s = "(]"
+     * - Output: false
+     */
+    private static boolean valid(String str) {
+
+        return false;
+    }
+
+}
