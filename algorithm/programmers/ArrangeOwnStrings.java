334de7ac7781243f26af7319cf58e91a6df13b26
221075 <221075@ssg.com>
2025-04-20 01:50:14 +0900

    feat : programmer - 문자열 내 마음대로 정렬하기

--- algorithm/programmers/ArrangeOwnStrings.java
+++ algorithm/programmers/ArrangeOwnStrings.java
@@ -4,8 +4,7 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 /**
- *
- * [ 문자열 내 마음대로 정렬하기 ]
+ * 문자열 내 마음대로 정렬하기
  *
  * [ 문제 설명 ]
  * 문자열로 구성된 리스트 strings와, 정수 n이 주어졌을 때, 각 문자열의 인덱스 n번째 글자를 기준으로 오름차순 정렬하려 합니다.
