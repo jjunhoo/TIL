a16d8c7e2a7a404b1dc1143efc6ceb28bac4716d
kimjunho <wnsghi1@naver.com>
2025-06-05 10:15:41 +0900

    coding test : 문제 풀이 노션 참고

---
+++ algorithm/how_about_here/First.java
@@ -0,0 +1,7 @@
+package algorithm.how_about_here;
+
+public class First {
+    public static void main(String[] args) {
+        // 노션 참고
+    }
+}
---
+++ algorithm/how_about_here/Third.java
@@ -0,0 +1,7 @@
+package algorithm.how_about_here;
+
+public class Third {
+    public static void main(String[] args) {
+        // 노션 참고
+    }
+}
