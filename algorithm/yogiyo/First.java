b0da068cbde4f342a6eb836e9ae06ab7f7c8cee0
kimjunho <wnsghi1@naver.com>
2025-06-05 10:18:03 +0900

    coding test : 문제 풀이 노션 참고

---
+++ algorithm/yogiyo/First.java
@@ -0,0 +1,7 @@
+package algorithm.yogiyo;
+
+public class First {
+    public static void main(String[] args) {
+        // 노션 참고
+    }
+}
---
+++ algorithm/yogiyo/Second.java
@@ -0,0 +1,7 @@
+package algorithm.yogiyo;
+
+public class Second {
+    public static void main(String[] args) {
+        // 노션 참고
+    }
+}
